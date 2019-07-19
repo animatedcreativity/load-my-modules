086c37d0a1b4d198ea6ee18b99f3492fa40cd669
Glitch (load-my-modules) <none>
2019-07-19 03:38:24 +0000

    🐣🎫 Updated with Glitch

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ exports = module.exports = function() {
   var fs = require("fs");
   var started = {};
   var mod = {
-    load: function(folder, app, moduleConfig) {
+    load: function(app, folder, moduleConfig) {
       if (typeof folder === "undefined") folder = "module";
       if (typeof moduleConfig === "undefined") moduleConfig = {};
       if (fs.existsSync(folder) === true) {
